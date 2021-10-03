5790f5d78b76f8613f359b42b1dde667872db3ec
Iwabuchi <yiwabuchi20@gmail.com>
2021-10-04 08:09:34 +0900

    feat: firebaseからデータを取得して、画面に表示した

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as firebase from "firebase";
+import "firebase/firestore";
+import { StyleSheet, Text, View } from "react-native";
+
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBwcHYhTu40WE3NmzRJ_O-BGKBVlRGcBx0",
+    authDomain: "shop-review-1e536.firebaseapp.com",
+    projectId: "shop-review-1e536",
+    storageBucket: "shop-review-1e536.appspot.com",
+    messagingSenderId: "224747186382",
+    appId: "1:224747186382:web:9702e353c22d0fbe65489f",
+    measurementId: "G-WV4ZTTK5CZ",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+}
+
+type Shop = {
+  name: string;
+  place: string;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [shops, setShops] = useState<Shop[]>([]);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const snapshot = await firebase.firestore().collection("shops").get();
+    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    setShops(shops);
+    console.log(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => (
+    <View style={{ margin: 10 }} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
     </View>
-  );
+  ));
+
+  return <View style={styles.container}>{shopItems}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
